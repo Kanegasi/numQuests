ec5a7fc8518b50391cb9501278c8ba60cdbc07a1
Kanegasi <kanegasi@gmail.com>
2022-11-06 12:49:15 -0500

    local working copy

--- nqcore.lua
+++ nqcore.lua
@@ -14,18 +14,21 @@ local WorldMapTitle = true
  -- ======== DO NOT EDIT ANYTHING BELOW THIS LINE ========
  -- ======================================================
 
-local InCombat,a,t=false,...
+local InCombat,q,a,t=false,{},...
 local f=CreateFrame("frame",a)
 local function p(m,r,g,b) ChatFrame1:AddMessage(m,r or 1,g or 1,b or 1,"temp") end
 SLASH_NUMQUESTS1="/numquests" SLASH_NUMQUESTS2="/numquest" SLASH_NUMQUESTS3="/numq"
 SlashCmdList["NUMQUESTS"]=function()
 	local other,counted,visible={},{},{}
+	zAddOnPocketTracker={}
 	for i=1,t.allmax do
-		local title,_,_,header,_,_,_,id,_,_,_,_,task,bounty,_,hidden=GetQuestLogTitle(i)
-		if title and not header then
-			local entry=(GetQuestLink(id) or title).." - ID: "..id
-			if hidden and (task or bounty) then tinsert(other,entry)
-			elseif hidden then tinsert(counted,entry)
+--		local title,_,_,header,_,_,_,id,_,_,_,_,task,bounty,_,hidden=GetQuestLogTitle(i)
+		q=C_QuestLog.GetInfo(i)
+		if q and q.title and not q.isHeader then
+			zAddOnPocketTracker[i]=q
+			local entry=(GetQuestLink(q.questID) or q.title).." - ID: "..q.questID
+			if q.isHidden and (q.isTask or q.isBounty) then tinsert(other,entry)
+			elseif q.isHidden then tinsert(counted,entry)
 			else tinsert(visible,entry) end
 		end
 	end
@@ -46,7 +49,7 @@ SlashCmdList["NUMQUESTS"]=function()
 end
 function t.PLAYER_REGEN_DISABLED() InCombat=true end function t.PLAYER_REGEN_ENABLED() InCombat=false end
 function t.QUEST_LOG_UPDATE()
-	local _,current=GetNumQuestLogEntries()
+	local _,current=C_QuestLog.GetNumQuestLogEntries()
 	local playermax=C_QuestLog.GetMaxNumQuestsCanAccept()
 	t.allmax=C_QuestLog.GetMaxNumQuests()
 	if current and playermax and not InCombat and not InCombatLockdown() then
@@ -56,5 +59,6 @@ function t.QUEST_LOG_UPDATE()
 		if WorldMapTitle then WorldMapFrame.BorderFrame.TitleText:SetText(MAP_AND_QUEST_LOG.." ("..curmax..")") end
 	end
 end
+hooksecurefunc(ObjectiveTrackerBlocksFrame.QuestHeader,"UpdateHeader",t.QUEST_LOG_UPDATE)
 f:RegisterEvent("PLAYER_REGEN_DISABLED") f:RegisterEvent("PLAYER_REGEN_ENABLED") f:RegisterEvent("QUEST_LOG_UPDATE")
 f:SetScript("OnEvent",function(_,event,...)t[event](...)end)
